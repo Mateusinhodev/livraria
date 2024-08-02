a37a6be75c32783c67af2c68b4ee992c33e22432
mateus <mateusdev23@gmail.com>
2024-08-02 17:57:26 -0300

    Linkando API do GoogleBooks

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,30 @@
+import { useEffect, useState } from 'react';
+import api from '../../services/api'
+
+
+// URL da API:  https://www.googleapis.com/books/v1/volumes?q=flowers&filter=free-ebooks&key=yourAPIKey
+// Configurada para o REACT: https://www.googleapis.com/books/v1/volumes?q=react&key=AIzaSyDXg_R6YQRMJGUi6SrSuettnTzj7WWxOFQ
+
+
 function Home() {
+    const [livros, setLivros] = useState([]);
+
+    useEffect(() => {
+        async function loadLivros() {
+            const response = await api.get("https://www.googleapis.com/books/v1/volumes?q=react&key", {
+                params: {
+                    api_key: "AIzaSyDXg_R6YQRMJGUi6SrSuettnTzj7WWxOFQ",
+                    language:"pt-BR",
+                }
+            })
+
+            console.log(response.data.items)
+        }
+
+        loadLivros();
+
+    }, [])
+
     return (
         <div>
             <h1>Bem vindo a HOME</h1>
---
+++ src/services/api.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+// Base da URL: https://www.googleapis.com/books/v1/volumes
+// URL da API:  https://www.googleapis.com/books/v1/volumes?q=react&key=AIzaSyDXg_R6YQRMJGUi6SrSuettnTzj7WWxOFQ
+
+const api =  axios.create({
+    baseURL: 'https://www.googleapis.com/books/v1/volumes?q=react&key'
+});
+
+export default api;
