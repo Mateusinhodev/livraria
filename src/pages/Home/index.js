d536d3559cbf852938955e0dab58176ef0c952a2
mateus <mateusdev23@gmail.com>
2024-08-06 14:39:11 -0300

    Listando as informações dos livros

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import api from '../../services/api'
-
+import { Link } from 'react-router-dom';
+import './home.css'
 
 // URL da API:  https://www.googleapis.com/books/v1/volumes?q=flowers&filter=free-ebooks&key=yourAPIKey
 // Configurada para o REACT: https://www.googleapis.com/books/v1/volumes?q=react&key=AIzaSyDXg_R6YQRMJGUi6SrSuettnTzj7WWxOFQ
@@ -19,6 +20,8 @@ function Home() {
             })
 
             console.log(response.data.items)
+
+            setLivros(response.data.items.slice(0,10))
         }
 
         loadLivros();
@@ -26,8 +29,21 @@ function Home() {
     }, [])
 
     return (
-        <div>
-            <h1>Bem vindo a HOME</h1>
+        <div className='container'>
+            <div className='lista-livros'>
+                {livros.map((livro) => {
+                    let thumbnail = livro.volumeInfo.imageLinks && livro.volumeInfo.imageLinks.smallThumbnail;
+                    return(
+                        <article key={livro.id}>
+                            <strong>{livro.volumeInfo.title}</strong>
+                            <img src={thumbnail}/>
+                            <Link to={`/livro/${livro.id}`}>Acessar</Link>
+                        </article>
+                    )
+                })
+
+                }
+            </div>            
         </div>
     )
 }
