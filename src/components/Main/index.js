a5185a9a959acaec61c6062cbd56e02188a8ea08
Mateus Dev <mateusdev23@gmail.com>
2024-12-02 11:49:40 -0300

    Implementando o Search

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,7 +1,16 @@
 import './main.css';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useState } from 'react';
 
 function Main() {
+
+    const [search, setSearch]=useState("");
+    const searchBook=(evt)=>{
+        if(evt.key==="Enter") {
+            console.log("Hello!");
+        }
+    }
     return(
         <>
             <div className='main'>
@@ -9,7 +18,8 @@ function Main() {
                 <div className='row2'>
                     <h2>Encontre seu livro</h2>
                     <div className='search'>
-                        <input type='text' placeholder='Digite o nome do seu livro'/>
+                        <input type='text' placeholder='Digite o nome do livro que procura...' value={search} onChange={e=>setSearch(e.target.value)} onKeyPress={searchBook}/>
+                        
                         <button>Pesquisar</button>
                     </div>
                     <img src='./imagens/livrariavirtual.png'/>
